ae4bd9d952fe8919d1ba20c24a97ab2f3ec616cc
costacoder <acosta.coder@gmail.comcleargit config --global user.email acosta.coder@gmail.com>
2025-01-20 22:10:17 -0300

    Iniciando o projeto

---
+++ tests/test_app.py
@@ -0,0 +1,17 @@
+from http import HTTPStatus
+
+from fastapi.testclient import TestClient
+
+from fast_zero.app import app
+
+client = TestClient(app)
+
+
+def test_read_root_deve_retornar_ok_e_ola_mundo():
+    client = TestClient(app)  # Arrange (organização)
+
+    response = client.get("/")  # Act (ação)
+
+    assert response.status_code == HTTPStatus.OK  # Assert (garantia)
+
+    assert response.json() == {'message': 'Olá mundo, ou helou uord'}  # Assert
